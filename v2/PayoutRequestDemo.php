5bfea5488480871b2c98df1d635fd8f0d83f6d39
moore <moore@tradev-group.com>
2024-07-01 16:05:54 +0800

    first commit

---
+++ v2/PayoutRequestDemo.php
@@ -0,0 +1,115 @@
+<?php
+
+$privateKeyStr = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC3LbmfPhjGXJ+a6NVKyRWEfCpsKfl9UFnMRltNosJv+7qd6OUK3t7Q8fKX8rxuJBeXLLWrOZvjoGlP3ybvvhUSGKH+BLcN1k2eJbcKuThvXvKQvd7pXolY91gkF8V78FX+TKegZupbeji0XUGXCBNyShocYM6Cvailf0Iyv49dcktJmi4drBKfgj5l4HUY9TaDuCZOrhvFZfZZxBK1zkm72k0ZLfqTGmG8O2tsByndCTH2aPLT9odR8/O4qfoTG+vV9HivIKKOI2h0kZfQBOqWD4/ofwu9PNoRRzGgzfRpF3GhnKa3bRVSpCUuBtHfotQfFxbjaDk0s3K5BhQE7HxzAgMBAAECggEARHhaBxUeC595pVzcxVyOp3wGG3JBKL9NIZc277kj9tngcsAoRTzziqS1qmh4WK8zBjYXHg6ln5tJYiqmkjy6AY6llp7KkeiGENRGLEL5vl9+Se4/EXpd2pxyHOOp1N8MNccPbVyqw1DXO0wUhVDme/UI94yUBLjB/kKoSvHhs+qwJ8cz9C8sF3Zs/zF6Te/f+Z+HrIbVj4vlx6DYBs3pWe7J+XYg3XXpbvIBJVTN0lreQAjtopic0F7o1EULnllJmOqy+kiRMuSi5ESrWFOnuCrY0iz/C8LJKlqWa3d+1jVLPvKYucXvddYrwjyu8kYHZAenKWKkLPlQkQxGUFcX8QKBgQDYSCsgg7tDlXTw1P9Z2BhkVo1ugYWA1FcxqTY2sTOMOwCTt3QZAFEqofbNvdjZbk7vu8gh5LyxbWAr/sNqOXH5915a6DZKBGl+qBTH2TZZKmhCUWlmn8T7ySYclfsgIZsUxHaDfY+otiXGtNegSX9US9/AFVSMdiQBCmr5/i48mQKBgQDY0U5+MDl6GnbDH/hCT+YMq/m5W58m7ehJfG6jPtLQkPPvMq2Oj5HUeXx4vDdUxzaRlC9fICwe2KgKiGYpJsRPte6JfO6te7wsO2lDk0oBiw+jewm4CwZ5KeDYyReha8Hc2H1j5hllVx5DIZiage2ZswS5+kCNgzf4QbdAOd886wKBgAo5TyCYWY/WTtLbnr6GgpCrrr/ci40NfJmyYAex1Lf6Sgqxj2FnLG8RfPM42DlfB4g5njpL78eLXhJ2VpJ86LBiSymM9JQHJV2BYIoZ8IHCiW8pHgxl3Q/x8EVFqbtZG1Wd++Q3WUUmZx6/ibnf/47ij08rMvX417bc4TW0GEdxAoGAPX3nUBynQH0e76oyg8QbT766nZphoe3ZcnYK/tuDeMmTlWR/Gq6XQnaOGcPvwWiajmFDqiv6t2jlB8+1gbhP9vd3RqEbJDKypKzY5uRwGc3xyoDLudnOpTB+Z51oyUxBeDwiG+IXk8lIeOufVzrAQ1YlYgWap0fu6MbijSGcsa8CgYEA0OolBxRKdBSxmwMbXDocg5HtHd15UgJdlgL9GFjDZPisUvM7LWK97E5QPhY2IEIcv0EW9jM62bc1uPMhwDuN1dzmLafQIivGsIj5qUy18CoE4KJHsTeKLeHkrd6/nBd4da4A37+ksJ1t/sgZpjl40ShrxTV+OtzzY/kuQ4Uqc9g=";
+$publicKeyStr = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAty25nz4YxlyfmujVSskVhHwqbCn5fVBZzEZbTaLCb/u6nejlCt7e0PHyl/K8biQXlyy1qzmb46BpT98m774VEhih/gS3DdZNniW3Crk4b17ykL3e6V6JWPdYJBfFe/BV/kynoGbqW3o4tF1BlwgTckoaHGDOgr2opX9CMr+PXXJLSZouHawSn4I+ZeB1GPU2g7gmTq4bxWX2WcQStc5Ju9pNGS36kxphvDtrbAcp3Qkx9mjy0/aHUfPzuKn6Exvr1fR4ryCijiNodJGX0ATqlg+P6H8LvTzaEUcxoM30aRdxoZymt20VUqQlLgbR36LUHxcW42g5NLNyuQYUBOx8cwIDAQAB";
+
+//url
+$payinPathUrl = "https://gateway-test.smilepayz.com/v2.0/disbursement/pay-out";
+$payinPathTestUrl = "https://sandbox-gateway-test.smilepayz.com/v2.0/disbursement/pay-out";
+
+echo "=====> step4 : TheSmilePay Payout" . PHP_EOL;
+
+//get time
+$currentTime = new DateTime('now', new DateTimeZone('UTC'));
+$currentTime->setTimezone(new DateTimeZone('Asia/Bangkok'));
+$timestamp = $currentTime->format('Y-m-d\TH:i:sP');
+
+//get merchantId from merchant platform
+$merchanteId = "20019";
+$merchanteIdsandbox = "sandbox-20019";
+
+$merchanteCode = "95b57c46b8c2e068982be23fb669a80612cad68e6ce6ba4f5af9ec20d23bb274";
+$merchanteCodeSandbox = "6a58a603e5043290f4097ee4a7745661b3656932d4eebc3106b5dddc3af6e053";
+
+//generate parameter
+// just for case. length less than 32
+$merchantOrderNo = "D_" . time();
+$purpose = "Purpose For Disbursement from PHP SDK";
+$paymentMethod = "YES";
+$cashAccount = "17385238451";
+$area = 12;
+
+//$moneyReq
+$moneyReq = array(
+    'currency' => 'INR',
+    'amount' => 200
+);
+
+//$merchantReq
+$merchantReq = array(
+    'merchantId' => $merchanteId
+);
+
+$additionalParam = array(
+    'ifscCode' => "YESB0000097"
+);
+//$payinReq
+$payinReq = array(
+    'orderNo' => $merchantOrderNo,
+    'purpose' => $purpose,
+    'money' => $moneyReq,
+    'additionalParam' => $additionalParam,
+    'merchant' => $merchantReq,
+    'paymentMethod' => $paymentMethod,
+    'cashAccount' => $cashAccount,
+    'area' => $area,
+
+);
+
+//json
+$jsonString = json_encode($payinReq);
+echo "jsonString=" . $jsonString . PHP_EOL;
+
+$referenceId = time();
+//build
+$stringToSign = $referenceId . "|" . $timestamp . "|" . $merchanteCode . "|" . $jsonString;
+echo "stringToSign=" . $stringToSign . PHP_EOL;
+
+
+//********** begin signature ***************
+// DER -> PEM
+$privateKeyPem = chunk_split($privateKeyStr, 64);
+$privateKeyPem = "-----BEGIN PRIVATE KEY-----\n" . $privateKeyPem . "-----END PRIVATE KEY-----\n";
+echo $privateKeyPem . PHP_EOL;
+
+$r = openssl_sign($stringToSign, $signature, $privateKeyPem, OPENSSL_ALGO_SHA256);
+$signatureValue = base64_encode($signature) ;
+echo "signatureValue=" . $signatureValue . PHP_EOL;
+//********** end signature ***************
+
+//********** begin post ***************
+
+// Create a cURL handle
+$ch = curl_init();
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_URL, $payinPathUrl);  // API URL
+curl_setopt($ch, CURLOPT_POST, true);  // POST
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonString);  // JSON Data
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'X-TIMESTAMP: ' . $timestamp,
+    'X-SIGNATURE: ' . $signatureValue,
+    'REFERENCE-ID: ' . $referenceId,
+    'X-PARTNER-ID: ' . $merchanteId,
+));
+
+// Execute the request and get the response
+$response = curl_exec($ch);
+echo $response . PHP_EOL;
+
+// Check for errors
+if ($response === false) {
+    echo 'cURL error: ' . curl_error($ch);
+} else {
+    // Process response result
+    echo PHP_EOL;
+    echo "response=" . $response . PHP_EOL;
+}
+
+// Close cURL handle
+curl_close($ch);
+
+echo "=====> Now. You get the AccessToken. So you can access other TheSmilePay API" . PHP_EOL;
